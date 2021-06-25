33bcd9b00f0740faee65ed0a3c36c0b29d8c5f4c
ty0x2333 <luckytianyiyan@gmail.com>
2021-06-25 23:25:31 +0800

    Add WheelSnapHelper

---
+++ WheelPicker/src/main/java/sh/tyy/wheelpicker/WheelSnapHelper.kt
@@ -0,0 +1,22 @@
+package sh.tyy.wheelpicker
+
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
+import androidx.recyclerview.widget.RecyclerView
+
+
+internal class WheelSnapHelper : LinearSnapHelper() {
+    override fun findSnapView(layoutManager: RecyclerView.LayoutManager?): View? {
+        return (layoutManager as? LinearLayoutManager)?.takeIf { isValidSnap(it) }
+            ?.run { super.findSnapView(layoutManager) }
+    }
+
+    private fun isValidSnap(linearLayoutManager: LinearLayoutManager): Boolean {
+        val firstCompletelyVisibleItemPosition =
+            linearLayoutManager.findFirstCompletelyVisibleItemPosition()
+        val lastCompletelyVisibleItemPosition =
+            linearLayoutManager.findLastCompletelyVisibleItemPosition()
+        return firstCompletelyVisibleItemPosition != 0 && lastCompletelyVisibleItemPosition != linearLayoutManager.itemCount - 1
+    }
+}
