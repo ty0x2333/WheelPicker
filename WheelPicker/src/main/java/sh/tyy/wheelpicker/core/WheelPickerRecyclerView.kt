497071054f89a58bc414871ed9cd6c4f8a1f0332
ty0x2333 <luckytianyiyan@gmail.com>
2021-07-06 13:37:52 +0800

    Optimize Code

--- WheelPicker/src/main/java/sh/tyy/wheelpicker/core/WheelPickerRecyclerView.kt
+++ WheelPicker/src/main/java/sh/tyy/wheelpicker/core/WheelPickerRecyclerView.kt
@@ -5,7 +5,6 @@ import android.graphics.Camera
 import android.graphics.Canvas
 import android.graphics.Matrix
 import android.util.AttributeSet
-import android.util.Log
 import android.view.HapticFeedbackConstants
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
